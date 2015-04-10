11bd796f595a7e077d3b134f59dbc2802dd56202
1101137101 <p19857@gmail.com>
2015-04-10 15:50:49 +0800

    first

---
+++ KuasCore/Dao/IEmployeeDao.cs
@@ -0,0 +1,24 @@
+﻿using KuasCore.Models;
+using System;
+using System.Collections.Generic;
+
+namespace KuasCore.Dao
+{
+    public interface IEmployeeDao
+    {
+
+        IList<Employee> GetAllEmployees();
+
+        Employee GetEmployeeById(string id);
+
+    }
+
+    public interface INewEmployeeDao
+    {
+
+        IList<Employee> GetAllEmployees();
+
+        Employee GetEmployeeById(string id);
+
+    }
+}
